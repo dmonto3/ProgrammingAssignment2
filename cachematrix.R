d42501f1b58d2cfa7c1e97c9829c46eaaa18798c
dmonto3 <dmon501@gmail.com>
2015-08-20 10:39:20 -0500

    special matrix created

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,18 @@
 ## This function creates a special "matrix" object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    inverse <- NULL
+    setMatrix <- function(y){
+        x <<- y
+        inverse <<- NULL
+    }
+    getMatrix <- function() x
+    setInverse <- function(inv) inverse <<- inv
+    getInverse <- function() inverse
+    list(setMatrix = setMatrix,
+         getMatrix = getMatrix,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
